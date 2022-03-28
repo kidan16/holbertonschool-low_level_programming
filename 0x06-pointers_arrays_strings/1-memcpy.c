82c1a1608514b555dc48b4798c5293b9cff9f35c
kidan16 <99591139+kidan16@users.noreply.github.com>
2022-03-28 15:43:47 +0300

    Update 1-memcpy.c

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * _memcpy - Copies memory area
- *
- * @dest: where to copy the memory to
- * @n: the number of bytes
- * @src: the memory area to copy from
- *
- * Return: a pointer to @dest
- */
+* _memcpy - Copies memory area
+*
+* @dest: where to copy the memory to
+* @n: the number of bytes
+* @src: the memory area to copy from
+*
+* Return: a pointer to @dest
+*/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
